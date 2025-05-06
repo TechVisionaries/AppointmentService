66802d1936d3d9a1cfcade01aa1af3c1f878aa50
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2025-05-06 19:43:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     );
 } else {
+    app.get('/', (req, res) => res.send('Server is ready!'));
     app.get('/api/appointments/health', (req, res) => res.send('Server is ready!'));
 }
 
